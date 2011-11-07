9ec5c51e5d21b01e143502b7f415905a8a01ad6f
Benjamin Esham <bdesham@gmail.com>
2011-11-07 14:17:31 -0600

    Add a script to bump the project's version

---
+++ bump_version.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# bump_version.sh
+#
+# Update the project's metadata for a new version
+#
+# Usage: bump_version.sh <major> <minor>
+#
+# e.g. bump_version.sh 2 0
+
+new_version="$1.$2"
+temp=`mktemp`
+
+gawk "
+	BEGIN {
+		found = 0
+	}
+
+	{
+		if (found == 1) {
+			print \"\t<string>\" $new_version \"</string>\"
+			found = 0
+		} else
+			print
+	}
+
+	/CFBundleShortVersionString/ || /CFBundleVersion/ {
+		found = 1
+	}
+" "CU Buses.dcproj/project/widget.wdgt/Info.plist" > $temp \
+	&& mv $temp "CU Buses.dcproj/project/widget.wdgt/Info.plist"
+
+sed -E -e "s/(widget_version_major = )[0-9]+;/\\1$1;/" \
+	-e "s/(widget_version_minor = )[0-9]+;/\\1$2;/" \
+	"CU Buses.dcproj/project/widget.wdgt/main.js" > $temp \
+	&& mv $temp "CU Buses.dcproj/project/widget.wdgt/main.js"
